e87869f947f36d2aaa5e7c85e8b7207b407ef022
Nb-Coding <128985854+Nb-Coding@users.noreply.github.com>
2024-12-20 14:51:19 +0900

    kadai_007のアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>PHPの基礎を学ぼう 8章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $item = [
+            'name' => 'onion',
+            'price' => 200,
+            'weight' => 160
+        ];
+
+        print_r($item);
+        ?>
+    </p>
+</body>
+
+</html>
